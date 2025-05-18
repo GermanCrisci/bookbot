619cf43f8cbf84734808656850bf68c8f694abda
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2025-05-18 16:33:02 -0300

    Refactoring

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(filepath):
+    """
+    Reads a text file and returns its content as a string.
+    
+    Args:
+        filepath (str): The path to the text file.
+        
+    Returns:
+        str: The content of the text file.
+    """
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+def main():
+    """
+    Main function to execute the script.
+    """
+    # Define the path to the text file
+    filepath = 'books/frankenstein.txt'
+    
+    # Get the content of the book
+    book_text = get_book_text(filepath)
+    
+    # Print the first 500 characters of the book
+    print(f"{get_num_words(book_text)} words found in the document.")
+    
+if __name__ == "__main__":
+    main()
